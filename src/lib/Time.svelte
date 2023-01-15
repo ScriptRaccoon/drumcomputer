66f412fe0012c2d3611cea91522ac408a012f6b9
Script Raccoon <scriptraccoon@gmail.com>
2023-01-15 10:01:33 +0100

    move Note component into Time component

--- src/lib/Time.svelte
+++ src/lib/Time.svelte
@@ -1,19 +1,44 @@
 <script lang="ts">
+	import { currentBeat, currentTime } from "@/ts/stores";
 	import { instruments } from "@/ts/instruments";
-	import Note from "@/lib/Note.svelte";
 	export let time: number;
 </script>
 
 <div class="time">
 	{#each instruments as instrument}
-		<Note {time} {instrument} />
+		<input
+			class="note"
+			type="checkbox"
+			value={instrument.name}
+			bind:group={$currentBeat.notes[time]}
+			class:current={$currentTime == time}
+		/>
 	{/each}
 </div>
 
-<style>
+<style lang="scss">
 	.time {
 		display: flex;
 		flex-direction: column;
 		gap: 2px;
+	}
+	.note {
+		appearance: none;
+		-webkit-appearance: none;
+		cursor: pointer;
+		width: min(40px, 10vw);
+		aspect-ratio: 1;
+		border-radius: 10%;
+		background-color: var(--note-color-off);
+		&:checked {
+			background-color: var(--note-color-on);
+		}
+		&:focus-visible {
+			z-index: 10;
+			outline: 1px solid white;
+		}
+		&.current {
+			filter: brightness(1.4);
+		}
 	}
 </style>
