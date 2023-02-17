547080f1778b7f7e56f1f35e77b9318133b24615
Script Raccoon <scriptraccoon@gmail.com>
2023-02-17 08:00:38 +0100

    loading  component

---
+++ src/components/Loading.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { instrumentsLoaded } from "@/ts/stores";
+</script>
+
+{#if !$instrumentsLoaded}
+	<p>Instruments are being loaded ...</p>
+{/if}
+
+<style>
+	p {
+		margin-top: 1rem;
+		color: var(--dark-font-color);
+		text-align: center;
+	}
+</style>
