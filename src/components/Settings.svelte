ee497282c8910a2f8ae4f5e282a39eae5e150209
Script Raccoon <scriptraccoon@gmail.com>
2023-02-18 21:57:49 +0100

    redesign of Settings component with grid

--- src/components/Settings.svelte
+++ src/components/Settings.svelte
@@ -92,34 +92,35 @@
 <h2>Settings</h2>
 
 <form on:submit|preventDefault>
-	<label>
-		<span>Scroll timeline</span>
-		<input type="checkbox" bind:checked={$timelineScrolls} />
+	<label for="scrollCheckbox"> Scroll timeline </label>
+	<input
+		id="scrollCheckbox"
+		type="checkbox"
+		bind:checked={$timelineScrolls}
+	/>
+	<label for="speedInput"> Note duration </label>
+
+	<input
+		id="speedInput"
+		class:error={error.noteDuration}
+		type="number"
+		min="1"
+		max="1000"
+		value={$currentBeat.noteDuration}
+		on:change={changeNoteDuration}
+	/>
+	<label for="divisionInput" aria-describedby="divisionDescription"
+		>Division<span aria-hidden="true">*</span>
 	</label>
-	<label>
-		<span>Note duration</span>
-		<input
-			class:error={error.noteDuration}
-			type="number"
-			min="1"
-			max="1000"
-			value={$currentBeat.noteDuration}
-			on:change={changeNoteDuration}
-		/>
-	</label>
-	<label>
-		<span aria-describedby="divisionDescription"
-			>Division<span aria-hidden="true">*</span></span
-		>
-		<input
-			class:error={error.division}
-			type="number"
-			min="1"
-			max="10"
-			value={$currentBeat.division}
-			on:change={changeDivision}
-		/>
-	</label>
+	<input
+		id="divisionInput"
+		class:error={error.division}
+		type="number"
+		min="1"
+		max="10"
+		value={$currentBeat.division}
+		on:change={changeDivision}
+	/>
 	<Button
 		disabled={Object.values(error).includes(true)}
 		action={hideSettings}
@@ -139,16 +140,26 @@
 		font-size: 2rem;
 		line-height: 1;
 		text-align: center;
+		margin-bottom: 2rem;
 	}
 
 	form {
-		display: flex;
-		flex-direction: column;
-		align-items: flex-start;
-		gap: 1rem;
-		margin-top: 2rem;
-		max-width: 20rem;
+		max-width: 16rem;
 		margin-inline: auto;
+		display: grid;
+		gap: 1rem;
+		grid-template-columns: 1fr 1fr;
+		justify-items: start;
+		align-items: center;
+
+		& :global(button) {
+			grid-column: 1 / span 2;
+			justify-self: center;
+		}
+
+		& .description {
+			grid-column: 1 / span 2;
+		}
 	}
 
 	input[type="checkbox"] {
@@ -177,14 +188,12 @@
 		}
 	}
 
-	label > span {
+	label {
+		font-size: 1.25rem;
 		color: var(--dark-font-color);
-		display: inline-block;
-		width: 6.5rem;
 	}
 
 	.description {
 		color: var(--dark-font-color);
-		font-size: smaller;
 	}
 </style>
