90173194fbb1436d2b2f202bbcfd346761fa7c61
Script Raccoon <scriptraccoon@gmail.com>
2023-02-25 15:42:56 +0100

    add github link

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,4 +1,17 @@
+<script lang="ts">
+	import Fa from "svelte-fa";
+	import { faGithub } from "@fortawesome/free-brands-svg-icons";
+</script>
+
 <header>
+	<a
+		href="https://github.com/ScriptRaccoon/drumcomputer"
+		target="_blank"
+		rel="noreferrer"
+		aria-label="GitHub repository"
+	>
+		<Fa aria-hidden icon={faGithub} />
+	</a>
 	<h1>Drum Computer</h1>
 </header>
 
@@ -6,6 +19,7 @@
 	header {
 		text-align: center;
 		padding: 1.5rem;
+		position: relative;
 	}
 
 	h1 {
@@ -15,6 +29,18 @@
 
 		@media (min-width: 420px) {
 			font-size: 2.5rem;
+		}
+	}
+
+	a {
+		position: absolute;
+		top: 0.5rem;
+		right: 0;
+		color: var(--dark-font-color);
+
+		&:focus {
+			outline: 0.1rem solid var(--font-color);
+			color: var(--font-color);
 		}
 	}
 </style>
